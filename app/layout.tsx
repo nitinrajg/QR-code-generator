40f319d338a7f474e94e1ee0ed4857225ff140fb
nitinrajg <vishwanathnitinraj796@gmail.com>
2025-04-10 23:09:40 +0530

    new3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import ClientLayout from '@/app/client-layout'
 import { ThemeProvider } from '@/components/theme-provider'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <ClientLayout>
             {children}
+            <SpeedInsights />
           </ClientLayout>
         </ThemeProvider>
       </body>
